e9d506f9b029a6121e3212c2e24620cfd97fd54a
רבקה קליין < @KITA-5-14>
2024-02-22 16:31:10 +0200

    Add project files.

---
+++ Generics/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Generics;
+Console.WriteLine("Hello, World!");
+
+static void PrintTheObj<T>(T obj)
+{
+    Console.WriteLine(obj);
+}
+PrintTheObj<int>(3);
+PrintTheObj<string>("hi");
+PrintTheObj<double>(1.2);
+
+SwapList<int> arr = new SwapList<int> { 1, 2, 3 };
+Console.WriteLine(arr[1]+" " + arr[2]);
+arr.MySwap(1,2);
+Console.WriteLine(arr[1] + " " + arr[2]);
+
+
---
+++ Generics/SwapList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class SwapList<T> : List<T>
+
+    {
+     
+        public SwapList() { }
+        public void MySwap(int i, int j)
+        {
+            T temp = this[i];
+            this[i] = this[j];
+            this[j] = temp;
+        }
+    }
+}
